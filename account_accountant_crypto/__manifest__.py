63ca6063f7bbcdbc4ab86829547fd9fe814d4beb
Yannis Burkhalter <ybu@open-net.ch>
2023-12-28 10:19:54 +0100

    [MIG] account_accountant_crypto: Migration to 15.0

--- account_accountant_crypto/__manifest__.py
+++ account_accountant_crypto/__manifest__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Crypto × Accounting",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "description":
         """
 Glue module to update crypto menu when account_accountant is installed.
 """,
     "author": "Yannis Burkhalter",
     "website":
-        "https://github.com/nymtech-odoo/crypto_kit/tree/14.0/account_accountant_crypto",
+        "https://github.com/nymtech-odoo/crypto_kit/tree/15.0/account_accountant_crypto",
     "license": "AGPL-3",
     "category": "Hidden",
     "depends": ["account_accountant", "crypto_kit"],
