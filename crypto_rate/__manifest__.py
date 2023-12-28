7c3dfdd1ce5023ce932dea8edbd4e83643992d3f
Yannis Burkhalter <ybu@open-net.ch>
2023-12-28 10:20:24 +0100

    [MIG] crypto_rate: Migration to 15.0

--- crypto_rate/__manifest__.py
+++ crypto_rate/__manifest__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 {
     "name": "CryptoRate",
-    "version": "14.0.0.2.0",
+    "version": "15.0.0.1.0",
     "description": """
 Base module for getting cryptocurrency rates in Odoo
 """,
     "author": "Yannis Burkhalter",
-    "website": "https://github.com/nymtech-odoo/crypto_kit/tree/14.0/crypto_rate",
+    "website": "https://github.com/nymtech-odoo/crypto_kit/tree/15.0/crypto_rate",
     "license": "AGPL-3",
     "category": "Accounting",
     "depends": ["crypto_kit"],
